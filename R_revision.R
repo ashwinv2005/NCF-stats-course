4cf478cbe9d28e7d9947e176e6124653cb1f7883
ashwinv2005 <40826026+ashwinv2005@users.noreply.github.com>
2023-06-23 12:41:13 +0530

    End of class 1

--- R_revision.R
+++ R_revision.R
@@ -1,2 +1,24 @@
 library(tidyverse)
 
+data(iris)
+
+iris.long = iris %>%
+  pivot_longer(cols = -Species, names_to = "temp", values_to = "Value") %>%
+  #separate(temp, into = c("part", "measure"))
+  separate_wider_delim(temp, ".", names = c("Part", "Measurement"))
+
+data = iris$Sepal.Length
+
+mn = numeric(1000)
+
+for (i in 1:1000)
+{
+  temp = sample(data,replace = T)
+  mn[i] = mean(temp)
+}
+
+lci = quantile(mn,0.025)
+mean = median(mn)
+rci = quantile(mn,0.975)
+
+mean(data)
